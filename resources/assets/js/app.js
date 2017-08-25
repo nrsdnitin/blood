94ead25087c00096a5c656725b3bb541db5b429f
nrsdnitin <nrsdpatel@gmail.com>
2017-08-25 12:43:51 +0100

    configure bulma buefy and fontawesome like basic setup and not to use bootstrap

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+/*import for custom js*/
+import Buefy from 'buefy';
+Vue.use(Buefy)
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
