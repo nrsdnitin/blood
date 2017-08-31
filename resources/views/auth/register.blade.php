a09306ef8d92b20ec87e3779a3c2e65b6e3e0823
nrsdnitin <nrsdpatel@gmail.com>
2017-08-31 16:05:14 +0100

    add correction on last commit for deleted email field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -107,7 +107,19 @@
                                 @endif
                             </div>
                         </div>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail</label>
 
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value=" " required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
