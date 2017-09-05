138e5c39a304457b707b50d691d6350a19dcfb17
Nitin Patel <nrsdpatel@gmail.com>
2017-09-05 23:12:00 +0100

    all well

--- app/Http/Controllers/HomeSearchController.php
+++ app/Http/Controllers/HomeSearchController.php
@@ -8,18 +8,10 @@ class HomeSearchController extends Controller
 {
   public function index(Request $request)
   {
-<<<<<<< HEAD
-
-=======
->>>>>>> commex
   //  dd($request->all());
     $user1 = DB::table('users')->where('blood_group', $request->blood_group)->first();
     //$valuee= HomeSearchController::all()->toArray();
     dd($user1);
-<<<<<<< HEAD
-    return view('home');
-=======
     return view('search.searchResult');
->>>>>>> commex
   }
 }
