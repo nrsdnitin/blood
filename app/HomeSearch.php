957be564116a2a99e479554215d6571c04f3a790
nrsdnitin <nrsdpatel@gmail.com>
2017-09-06 11:58:40 +0100

    search process complete

---
+++ app/HomeSearch.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HomeSearch extends Model
+{
+  protected $table = 'users';
+    //
+}
--- app/Http/Controllers/HomeSearchController.php
+++ app/Http/Controllers/HomeSearchController.php
@@ -3,15 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+ use App\HomeSearch;
 use DB;
 class HomeSearchController extends Controller
 {
   public function index(Request $request)
   {
-  //  dd($request->all());
-    $user1 = DB::table('users')->where('blood_group', $request->blood_group)->first();
+    $searchResult=HomeSearch::where('blood_group', '=',  $request->blood_group)
+                //->where('type', '=', 1)
+                //->where('is_active', '=', 1)
+                ->select('name', 'email')
+                ->get();
+
+  /*  foreach ($searchResult as $user) {
+                    echo $user->name;
+        }
+*/
+
+
+  //  dd($searchResult);
+  //  $user1 = DB::table('users')->where('blood_group', $request->blood_group);
     //$valuee= HomeSearchController::all()->toArray();
-    dd($user1);
-    return view('search.searchResult');
+  //  dd($user1);
+  //  return view('search.searchResult');
+    return view('search.searchResult', ['users' => $searchResult]);
   }
 }
